31833be573ed4eadaed78e29d618b719e35854c6
Kirill <skaweb@yandex.ru>
2026-07-20 19:38:03 +0300

    Добавить события в BasketView

--- src/components/views/BasketView.ts
+++ src/components/views/BasketView.ts
@@ -1,5 +1,6 @@
 import { ensureElement } from "../../utils/utils";
 import { Component } from "../base/Component";
+import { IEvents } from "../base/Events";
 
 interface BasketViewData {
   purchases: HTMLElement[];
@@ -7,23 +8,38 @@ interface BasketViewData {
 }
 
 export class BasketView extends Component<BasketViewData> {
-  
   private completeButton: HTMLButtonElement;
   private shoppingListEl: HTMLElement;
   private totalCostEl: HTMLElement;
-  
-  constructor(container: HTMLElement) {
+
+  constructor(
+    container: HTMLElement,
+    private events: IEvents,
+  ) {
     super(container);
-    this.completeButton = ensureElement<HTMLButtonElement>('.basket__button', this.container)
-    this.shoppingListEl = ensureElement<HTMLElement>('.basket-list', this.container)
-    this.totalCostEl = ensureElement<HTMLElement>('.basket-price', this.container)
+    this.completeButton = ensureElement<HTMLButtonElement>(
+      ".basket__button",
+      this.container,
+    );
+    this.shoppingListEl = ensureElement<HTMLElement>(
+      ".basket-list",
+      this.container,
+    );
+    this.totalCostEl = ensureElement<HTMLElement>(
+      ".basket-price",
+      this.container,
+    );
+
+    this.completeButton.addEventListener("click", () => {
+      this.events.emit("order:edit");
+    });
   }
 
   set purchases(purchases: HTMLElement[]) {
-    this.shoppingListEl.replaceChildren(...purchases)
+    this.shoppingListEl.replaceChildren(...purchases);
   }
 
   set totalCost(value: number) {
-    this.totalCostEl.textContent = `${value ?? 0} синапсов`
+    this.totalCostEl.textContent = `${value ?? 0} синапсов`;
   }
 }
