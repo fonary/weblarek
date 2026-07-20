f20f0777a12e9fb12335257a909420ab02f2ace3
Kirill <skaweb@yandex.ru>
2026-07-20 21:47:35 +0300

    Добавить обработчик SuccessView

--- src/components/views/SuccessView.ts
+++ src/components/views/SuccessView.ts
@@ -1,5 +1,6 @@
 import { ensureElement } from "../../utils/utils";
 import { Component } from "../base/Component";
+import { IEvents } from "../base/Events";
 
 interface SuccessViewData {
   amount: number;
@@ -9,16 +10,23 @@ export class SuccessView extends Component<SuccessViewData> {
   private closeButton: HTMLButtonElement;
   private debitEl: HTMLElement;
 
-  constructor(container: HTMLElement) {
+  constructor(
+    container: HTMLElement,
+    private events: IEvents,
+  ) {
     super(container);
     this.closeButton = ensureElement<HTMLButtonElement>(
       ".order-success__close",
       this.container,
     );
     this.debitEl = ensureElement(".order-success__description", this.container);
+
+    this.closeButton.addEventListener("click", () => {
+      this.events.emit("modal:close");
+    });
   }
 
   set amount(amount: number) {
-    this.debitEl.textContent = `Списано ${amount} синапсов`
+    this.debitEl.textContent = `Списано ${amount} синапсов`;
   }
 }
